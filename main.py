f1298265ac09d8af34825fd20fab3a7804aef858
Dimitris Mouris <dimitris@nillion.com>
2024-12-02 16:44:59 -0500

    Add secret sharing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
+from secret_sharing import *
 
 # Load text from file
 def load_file(file_path):
@@ -34,8 +35,14 @@ def find_closest_chunks(query_embedding, chunks, embeddings, top_k=2):
     sorted_indices = np.argsort(distances)
     return [(chunks[idx], distances[idx]) for idx in sorted_indices[:top_k]]
 
+# Find the closest chunks to a query with secret sharing
+# def find_closest_chunks_private(query_embedding, chunks, embeddings, top_k=2):
+    # distances = [euclidean_distance(query_embedding, emb) for emb in embeddings]
+    # sorted_indices = np.argsort(distances)
+    # return [(chunks[idx], distances[idx]) for idx in sorted_indices[:top_k]]
 
-def main():
+
+def rag_plaintext():
     file_path = "data.txt"
 
     # Load paragraphs and preprocess
@@ -46,7 +53,7 @@ def main():
     embeddings = generate_embeddings_huggingface(chunks)
 
     # User Query
-    query = "Tell me about places in Japan."
+    query = "Tell me about places in Asia."
     query_embedding = generate_embeddings_huggingface([query])[0]
 
     # Retrieve top results
@@ -58,6 +65,56 @@ def main():
     for i, (chunk, dist) in enumerate(top_results, 1):
         print(f"{i}. {chunk} (Distance: {dist:.2f})")
 
+def rag_with_shares():
+    precision = 7
+    secret_sharing = AdditiveSecretSharing(3, 2**31 - 1, precision)
+
+    file_path = "data.txt"
+
+    # Load paragraphs and preprocess
+    paragraphs = load_file(file_path)
+    chunks = create_chunks(paragraphs, chunk_size=50, overlap=10)  # Reduced chunk size for clarity
+
+    # Generate embeddings
+    embeddings = generate_embeddings_huggingface(chunks)
+    embedding_shares = []
+    for embedding in embeddings:
+        embedding_shares.append(secret_sharing.secret_share(embedding))
+
+    # TODO(@jimouris): fix negatives
+    # reconstructed_embeddings = []
+    # for embedding_share in embedding_shares:
+    #     reconstructed_embeddings.append(secret_sharing.secret_reveal(embedding_share))
+    # for i in range(len(embeddings)):
+    #     for j in range(len(embeddings[i])):
+    #         e = round(float(embeddings[i][j]), precision)
+            # print(e, reconstructed_embeddings[i][j])
+            # assert e == reconstructed_embeddings[i][j]
+
+    embedding_length = len(embeddings[0])
+
+    # User Query
+    query = "Tell me about places in Asia."
+    query_embedding = generate_embeddings_huggingface([query])[0]
+    query_embedding_shares = secret_sharing.secret_share(query_embedding)
+
+    # # Retrieve top results
+    # top_results = find_closest_chunks_private(query_embedding_shares, chunks, embedding_shares)
+
+    # # Display results
+    # print("Query:", query)
+    # print("\nTop Matches:")
+    # for i, (chunk, dist) in enumerate(top_results, 1):
+    #     print(f"{i}. {chunk} (Distance: {dist:.2f})")
+
 
 if __name__ == "__main__":
-    main()
+    # secret_sharing_test()
+    # print()
+
+    # rag_plaintext()
+    # print()
+
+    rag_with_shares()
+    print()
+
