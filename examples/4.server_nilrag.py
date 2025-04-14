ebf149b020ffa5be297b1e6531591f6104cb04dc
jfdreis <josevtnreis@gmail.com>
2025-04-14 13:01:25 +0100

    chore: update code and README.md according to reviews

--- examples/4.server_nilrag.py
+++ examples/4.server_nilrag.py
@@ -9,7 +9,6 @@ import time
 
 from nilrag.config import load_nil_db_config
 from nilrag.nildb_requests import ChatCompletionConfig
-from nilrag.util import generate_embeddings_huggingface
 
 DEFAULT_CONFIG = "examples/nildb_config.json"
 DEFAULT_PROMPT = "Who is Danielle Miller?"
@@ -51,27 +50,9 @@ async def main():
     print(nil_db)
     print()
 
-    #Generate query embedding
-    start_time = time.time()
-    query_embedding = generate_embeddings_huggingface(args.prompt)
-    end_time = time.time()
-    embedding_query_generation_time = round(end_time - start_time, 2)
-    print(f"Embedding query generation time: {embedding_query_generation_time} sec")
-
-    # Check if clustering was performed
-    print("Starting cluster check...")
-    start_time = time.time()
-    num_clusters, closest_centroid = await nil_db.get_closest_centroid(query_embedding)
-    cluster_check_time = time.time() - start_time
-    if num_clusters > 1 and closest_centroid is not None:
-        print(f"Clustering was performed - found {num_clusters} clusters and closest centroid to query")
-    else:
-        print("No clustering was performed")
-    print(f"Cluster check and (if existing) centroid selection took {cluster_check_time:.2f} seconds")
-
     print("Perform nilRAG...")
     start_time = time.time()
-    top_chunks = await nil_db.top_num_chunks_execute(query_embedding, 2, closest_centroid)
+    top_chunks = await nil_db.top_num_chunks_execute(args.prompt, 2)
     end_time = time.time()
     print(json.dumps(top_chunks, indent=4))
     print(f"Query took {end_time - start_time:.2f} seconds")
