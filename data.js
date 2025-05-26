b49c82fa7ad84027b742daeff5ac7cde9ac98a41
github-action-benchmark <github@users.noreply.github.com>
2025-05-26 15:38:03 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 8f6b816cf8361407339f2ac4c112b563813acc93

---
+++ data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1748273883252,
+  "repoUrl": "https://github.com/NillionNetwork/nilrag",
+  "entries": {
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "NillionNetwork",
+            "username": "NillionNetwork"
+          },
+          "committer": {
+            "name": "NillionNetwork",
+            "username": "NillionNetwork"
+          },
+          "id": "8f6b816cf8361407339f2ac4c112b563813acc93",
+          "message": "Feat/benches pipeline",
+          "timestamp": "2025-05-19T16:29:38Z",
+          "url": "https://github.com/NillionNetwork/nilrag/pull/21/commits/8f6b816cf8361407339f2ac4c112b563813acc93"
+        },
+        "date": 1748273882369,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/test_rag.py::test_rag_pedantic",
+            "value": 0.12840434662979064,
+            "unit": "iter/sec",
+            "range": "stddev: 0.058713964540769915",
+            "extra": "mean: 7.787898355833335 sec\nrounds: 3"
+          }
+        ]
+      }
+    ]
+  }
+}
